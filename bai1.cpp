622cb8bbf0e96107021208a531473346db4319dd
Km27121212 <khaiminh05.cn@gmail.com>
2025-11-12 08:04:00 +0700

    Add files via upload

---
+++ bai1.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    printf("Chao cac ban! Toi la Nguyen Pham Khai Minh\n");
+    printf("Ma sinh vien: PS48030\n");
+    printf("Toi den tu Da Nang\n");
+    return 0;
+}
